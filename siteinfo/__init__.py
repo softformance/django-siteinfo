f7e09157484dff6e9fc497912d794d8d65cd6847
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2019-03-29 15:58:17 +0200

    Add apps.py file

--- siteinfo/__init__.py
+++ siteinfo/__init__.py
@@ -1,2 +1,3 @@
 VERSION = (0, 9, 15, 1)
 __version__ = '0.9.15.1'
+default_app_config = 'siteinfo.apps.Siteinfo'
---
+++ siteinfo/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class Siteinfo(AppConfig):
+    name = 'siteinfo'
+    verbose_name = 'Siteinfo'
