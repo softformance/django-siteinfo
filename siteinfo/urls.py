52a34112b4ef3469ce1b6d7cb4f69bf8e323529e
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2019-03-29 15:39:57 +0200

    Compatibility for django-siteinfo at urls.py file

--- siteinfo/urls.py
+++ siteinfo/urls.py
@@ -1,11 +1,8 @@
-
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
-
+import django
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
+from siteinfo import views as siteinfo_views
 from siteinfo.models import SiteSettings
 
 site_settings = SiteSettings.objects.get_current()
@@ -16,10 +13,39 @@ else:
     agb_url = ""
 
 
-urlpatterns = [
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login_url'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout_url'),
-    url(r'^agb/?$', "django.views.generic.simple.redirect_to", {'url': agb_url}, name="agb"),
-    url(r'^srs/?$', 'siteinfo.views.set_redirect_stop', name='set_redirect_stop'),
-    url(r'^test_offline_page/?$', 'siteinfo.views.test_offline_page', name='siteinfo-test_offline_page'),
-]
+if django.VERSION < (1, 8):
+    try:
+        from django.conf.urls.defaults import patterns, url
+    except:
+        from django.conf.urls import patterns, url
+
+    urlpatterns = patterns('',
+                           url(r'^login/$', auth_views.login, name='login_url'),
+                           url(r'^logout/$', auth_views.logout, name='logout_url'),
+
+                           url(r'^srs/?$', siteinfo_views.set_redirect_stop, name='set_redirect_stop'),
+                           url(r'^test_offline_page/?$', siteinfo_views.test_offline_page,
+                               name='siteinfo-test_offline_page'),
+                           )
+    if django.VERSION < (1, 5):
+        urlpatterns += patterns('',
+            url(r'^agb/?$', django.views.generic.simple.redirect_to, {'url': agb_url}, name="agb"),
+        )
+    else:
+        from django.views.generic.base import RedirectView
+
+        urlpatterns += patterns('',
+            url(r'^agb/?$', RedirectView.as_view(url=agb_url), name="agb"),
+        )
+
+else:
+    from django.conf.urls import url
+    from django.views.generic import RedirectView
+
+    urlpatterns = [
+        url(r'^login/$', auth_views.login, name='login_url'),
+        url(r'^logout/$', auth_views.logout, name='logout_url'),
+        url(r'^agb/?$', RedirectView.as_view(url=agb_url), name="agb"),
+        url(r'^srs/?$', siteinfo_views.set_redirect_stop, name='set_redirect_stop'),
+        url(r'^test_offline_page/?$', siteinfo_views.test_offline_page, name='siteinfo-test_offline_page'),
+    ]
